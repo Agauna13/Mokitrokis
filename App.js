735bfd09d3271393075e2036e5bf3fea88eff997
Joch1986 <149946304+Joch1986@users.noreply.github.com>
2024-07-16 23:00:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsuariosComponent from './UsuariosComponent';
+import './App.css';
+const App = () => {
+  return(
+    <div className='App'>
+      <h1>Mokitrokis</h1>
+      <UsuariosComponent/>
+    </div>
+  )
+}
+
+export default App;
