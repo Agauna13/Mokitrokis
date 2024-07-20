5c92bdaadcfc9df51a71fcf1dac8ff66deb015fc
Agauna13 <agauna@cifpfbmoll.eu>
2024-07-20 20:28:27 +0200

    ñjh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Presentation from './components/Presentation';
+import Reservations from './components/Reservations';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Presentation />
+            <Menu />
+            <Reservations />
+        </>
+    );
+};
 
 export default App;
